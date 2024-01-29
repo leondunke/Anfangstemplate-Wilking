637f78266d54fee9160078f0a72e6054a7aef433
leondunke <156666082+leondunke@users.noreply.github.com>
2024-01-29 11:25:02 +0100

    Dies ist mein erster commit

---
+++ Hello_World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int x;
+float y = 2.45;
+
+
+printf("Hello World!\n"); /*Das hier ist ein Kommentar. Ich gebe "Hello World!" aus*/
+printf("Zwei Mal y plus x ergibt:")
+
+return 0;
+}
