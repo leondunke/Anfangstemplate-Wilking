dec40db74cff54f25fb2357dbb21c7950a3cce3a
leondunke <156666082+leondunke@users.noreply.github.com>
2024-01-29 11:35:55 +0100

    Das ist mein zweiter Commit Hier spiele ich mit Variablen herum

--- Hello_World.c
+++ Hello_World.c
@@ -3,10 +3,10 @@ int main()
 {
 int x;
 float y = 2.45;
-
-
+x = y / 3.56;
+y = y * (2 + x);
 printf("Hello World!\n"); /*Das hier ist ein Kommentar. Ich gebe "Hello World!" aus*/
-printf("Zwei Mal y plus x ergibt:")
+printf("Zwei Mal y plus x ergibt: %f\n", y);
 
 return 0;
 }
