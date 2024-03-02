c40af8a06a3f8da30a2638667100521da69d9cbb
leondunke <156666082+leondunke@users.noreply.github.com>
2024-03-02 12:57:12 +0100

    Hausaufgabe 2 ist fertig

--- Leon_Semester_1/Homework_2.c
+++ Leon_Semester_1/Homework_2.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main()
 {
-int values[] = {2, 4, 6, 1, 1};
-float a = (values[0] + values[1] + values[2] + values[3] + values[4]) / 5;
+int values[] = {1, 2, 3, 4, 6};
+float a = (float)(values[0] + values[1] + values[2] + values[3] + values[4]) / 5;
 printf("Hello World!\n"); /*Das hier ist ein Kommentar. Ich gebe "Hello World!" aus*/
-printf("Der Durchschnitt lautet: %f\n", a);
+printf("Der Durchschnitt lautet: %.1f\n", a);
+
+int v = 189;
+int p = 24;
+float r = (float)v*p/100;
+printf("%i Prozent von %i ist %.2f %%\n", p, v, r);
 
 return 0;
 }
