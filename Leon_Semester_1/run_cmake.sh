b3f920d797c3c5149b83f5cf400eff5c8e44ecb5
leondunke <156666082+leondunke@users.noreply.github.com>
2024-03-02 11:51:29 +0100

    Erster Versuch fehlgeschlagen, Versuch 2

--- Leon_Semester_1/run_cmake.sh
+++ Leon_Semester_1/run_cmake.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake -S. -B build -DCMAKE_BUILD_TYPE=DEBUG -G "Unix Makefiles" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=gcc++
+cmake -S. -B build -DCMAKE_BUILD_TYPE=DEBUG -G "Unix Makefiles" -D CMAKE_C_COMPILER=gcc
