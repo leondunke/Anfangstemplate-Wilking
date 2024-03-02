57d8da7684071a3686f54f5c437c6602d7717ec9
leondunke <156666082+leondunke@users.noreply.github.com>
2024-03-02 11:00:08 +0100

    Ich habe den Wert der Variablen geändert

--- Leon_Semester_1/Hello_World.c
+++ Leon_Semester_1/Hello_World.c
@@ -2,7 +2,7 @@
 int main()
 {
 float x;
-float y = 2.45;
+float y = 2.4;
 x = y / 3.56;
 y = y * (2 + x);
 printf("Hello World!\n"); /*Das hier ist ein Kommentar. Ich gebe "Hello World!" aus*/
--- Leon_Semester_1/cmake_install.cmake
+++ Leon_Semester_1/cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "DEBUG")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/workspaces/Anfangstemplate-Wilking/Leon_Semester_1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/workspaces/Anfangstemplate-Wilking/Leon_Semester_1/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
