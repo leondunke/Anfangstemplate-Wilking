3282ac79b5e78e7bf0a2a06b6446a4fc51e61e27
leondunke <156666082+leondunke@users.noreply.github.com>
2024-03-02 11:45:27 +0100

    Neuhochladen der CMake Sachen1

---
+++ Leon_Semester_1/Hello_World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+float x;
+float y = 2.45;
+x = y / 3.56;
+y = y * (2 + x);
+printf("Hello World!\n"); /*Das hier ist ein Kommentar. Ich gebe "Hello World!" aus*/
+printf("Zwei Mal y plus x ergibt: %f\n", y);
+
+return 0;
+}
