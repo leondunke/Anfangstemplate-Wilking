ca41900bdd7c2e69f31dc815c11e4ee178723b61
leondunke <156666082+leondunke@users.noreply.github.com>
2024-03-10 21:53:17 +0100

    Hausaufgaben 1-5 Vollständig erledigt

--- Leon_Semester_1/Homework/Homework_5_Triangle.c
+++ Leon_Semester_1/Homework/Homework_5_Triangle.c
@@ -4,8 +4,11 @@ int main()
     int h;
     int i;
     int k;
-    printf("Geben Sie die gewünschte Höhe des Dreiecks an: ");
-    scanf("%d", h);
+    int j;
+    int z;
+    int y =0;
+    printf("Geben Sie die gewünschte Höhe des Dreiecks als ganze Zahl an: ");
+    scanf("%i", &h);
     for (i = 0; i < h; i++)
     {
         for (k = 0; k < i+1; k++)
@@ -14,6 +17,25 @@ int main()
         }
         printf("\n");
     }
+    printf("\n");
+    i = 0; 
+    for (i = 0; i < h; i++)
+    {
+        for (j=0;j <= k-1; j++)
+        {
+           printf(" ");
+        }   
+        k--;
+        
+        for (z = 0; z < y+1; z++)
+        {
+           printf("*");
+        }
+        y = y+2;
+        printf("\n");
+    }
     
+
+    printf("\n");
     return 0;
 }
